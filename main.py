fa1d182233f3503254147c61ac4cdf07fab6fbde
s28925 <s28925@pjwstk.edu.pl>
2024-03-24 20:53:06 +0100

    Task 2 completed

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 
 squared_numbers = [n * n for n in range(1, 11)]
 print(squared_numbers)
+
+# Task 2: Functions
+
+
+def e_squares(start, end):
+    result = [n * n for n in range(start, end)]
+    return result
+
+
+print(e_squares(1, 11))
