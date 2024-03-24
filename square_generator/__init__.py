f5d6cc9bcfbadae673b3b2b466b5ca622acb83c5
s28925 <s28925@pjwstk.edu.pl>
2024-03-24 21:52:12 +0100

    Task 9 completed

--- square_generator/__init__.py
+++ square_generator/__init__.py
@@ -1,6 +1,9 @@
 # Task 6: Modules
 # Task 7: Packages
 
+from typing import override
+
+
 class SquareGenerator:
     def generate_squares(self, start, end):
 
@@ -16,6 +19,16 @@ class SquareGenerator:
 
 
 class CubicGenerator(SquareGenerator):
+
+    # Task 9: Function Overriding
+    @override
+    def generate_squares(self, start, end):
+        if start < end:
+            result = [n * n for n in range(start, end)]
+            return result
+        else:
+            raise ValueError("The start of the range cannot be higher than the end")
+
     def generate_cubes(self, start, end):
         if start > end:
             print("The start of the range cannot be higher than the end of the range")
